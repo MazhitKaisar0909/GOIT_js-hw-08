49d3c1e1d66c6fe128a29ae8b1022527fd84f62b
Mazhit <kajsar02@gmail.com>
2023-08-16 21:55:37 +0300

    second part

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('loaded', function () {
+    console.log('Player is loaded');
+
+    player.on('play', function () {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function (title) {
+        console.log('title:', title);
+    });
+});
